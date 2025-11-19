bea7d1e4bd89752206232a06548bc4a2edbba0a9
dawidsajewski12-creator <dawidsajewski12@gmail.com>
2025-11-19 17:56:44 +0100

    Delete server.py

--- server.py
+++
@@ -1,5 +0,0 @@
-import http.server; import socketserver
-PORT = 8000; Handler = http.server.SimpleHTTPRequestHandler
-with socketserver.TCPServer(("", PORT), Handler) as httpd:
-    print(f"Serwer uruchomiony. Otwórz w przeglądarce: http://localhost:{PORT}")
-    httpd.serve_forever()
